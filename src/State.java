26d88f786a9fe14d60399c61c69b2b2cecb92b29
nicolas_alahmar <nicolasalahmar@gmail.com>
2022-12-27 01:36:06 +0300

    added getNextStates, can't test before we implement a real problem and fill all static fields.

--- src/State.java
+++ src/State.java
@@ -7,32 +7,61 @@ import java.util.Objects;
 
 public class State {
     State parent;
-    static float walking_speed= 5.5F;
-    static Map<String,float[]> stations;
-    static int bus_fee=400;
-    static int taxi_fee=1000;
-    static int walking_fee=0;
+    static float walking_speed = 5.5F;
+    static Map<String, float[]> stations;   // i think float instead of Float will make some problems
+    static int bus_fee = 400;
+    static int taxi_fee = 1000;
+    static int walking_fee = 0;
     float balance;
     int HP;
-    static int bus_energy_cost=-5;
-    static int taxi_energy_cost=5;
-    static int walking_energy_cost=-10;
-    static Map<String,Map<String,Edge>> edges;
+    static int bus_energy_cost = -5;
+    static int taxi_energy_cost = 5;
+    static int walking_energy_cost = -10;
+    static Map<String, Map<String, Edge>> edges;
     String currentStation;
 
-    public State(State parent, float balance, int HP,String currentStation) {
+    public State(State parent, float balance, int HP, String currentStation) {
         this.parent = parent;
         this.balance = balance;
         this.HP = HP;
-        this.currentStation=currentStation;
+        this.currentStation = currentStation;
     }
 
-    public Map<String,Edge> checkMoves(Map<String,Map<String,Edge>> edges) {
-        return  edges.get(currentStation);
+    public Map<String, Edge> checkMoves() {
+        return State.edges.get(currentStation);
     }
 
+    public State[] getNextStates() {
+        Map<String, Edge> moves = checkMoves();
+        State[] result = new State[moves.size() * 3];
+        int i = 0;
+        for (Map.Entry<String, Edge> entry : moves.entrySet()) {
+            Edge e = entry.getValue();
+            if (HP + (State.walking_energy_cost * e.distance) > 0
+                    && balance - State.walking_fee > 0) {// you can walk
+                result[i] = new State(this, balance,
+                        (int) (HP + (State.walking_energy_cost * e.distance)), entry.getKey());
+                i++;
+            }
 
+            if (e.bus_route && HP + (State.bus_energy_cost * e.distance) > 0
+                    && balance - State.bus_fee > 0) {// you can take the bus (there is bus route/
+                                                     // hp allows it /balance allows it)
+                result[i] = new State(this, balance,
+                        (int) (HP + (State.bus_energy_cost * e.distance)), entry.getKey());
+                i++;
+            }
+
+            if (e.taxi_route && HP + (State.taxi_energy_cost * e.distance) > 0
+                    && balance - (State.taxi_fee * e.distance) > 0) {// you can take a taxi (there
+                                                                     // is taxi route/ hp allows it
+                                                                     // /balance allows it)
+                result[i] = new State(this, balance,
+                        (int) (HP + (State.taxi_energy_cost * e.distance)), entry.getKey());
+                i++;
+            }
+        }
+        return result;
+    }
 
 }
-
-
