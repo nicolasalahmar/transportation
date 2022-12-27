2083c92239825eaf3bdfad059bf8623a9189a16f
nicolas_alahmar <nicolasalahmar@gmail.com>
2022-12-27 23:27:51 +0300

    added new constructor to edge and added some stations without correct values.

--- src/Edge.java
+++ src/Edge.java
@@ -12,4 +12,11 @@ class Edge {
         bus_speed = 0;
         taxi_speed = 0;
     }
+    public Edge(int distance, boolean bus_route, boolean taxi_route, float bus_speed, float taxi_speed){
+        this.distance = distance;
+        this.bus_route = bus_route;
+        this.taxi_route = taxi_route;
+        this.bus_speed = bus_speed;
+        this.taxi_speed = taxi_speed;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        State s = new State(null, 10000, 100, "m1");
+        //this.stations.put(station name, [bus waiting time, taxi waiting time])
+        State.stations.put("m1", new Double[]{ 4.5, 1.5});
+        State.stations.put("m2", new Double[]{ 4.5, 1.5});
+        State.stations.put("m3", new Double[]{ 4.5, 1.5});
+        State.stations.put("m4", new Double[]{ 4.5, 1.5});
+        State.stations.put("m5", new Double[]{ 4.5, 1.5});
+
+        // State.edges.put
     }
 }
