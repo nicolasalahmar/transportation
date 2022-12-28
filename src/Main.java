8fcc5753fb836bc6fdb82ba346b5d77ee4a33151
GeoN <geon200181@gmail.com>
2022-12-28 08:02:54 +0200

    Updates By George
    I've flipped all attributes in Edge class to double ,
    I've changed Maps in State.java to HashMap,
    I've added additional information about the state in Main.java file.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,63 @@
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
         State s = new State(null, 10000, 100, "m1");
         //this.stations.put(station name, [bus waiting time, taxi waiting time])
-        State.stations.put("m1", new Double[]{ 4.5, 1.5});
-        State.stations.put("m2", new Double[]{ 4.5, 1.5});
-        State.stations.put("m3", new Double[]{ 4.5, 1.5});
-        State.stations.put("m4", new Double[]{ 4.5, 1.5});
-        State.stations.put("m5", new Double[]{ 4.5, 1.5});
+
+        State.stations = new HashMap<String, Double[]>();
+        State.stations.put("m1", new Double[]{ 4.5, 1.5});//we'll consider m1 is IT Faculty
+        State.stations.put("m2", new Double[]{ 4.5, 1.5});//we'll consider m2 is Bab Sharqi Area
+        State.stations.put("m3", new Double[]{ 4.5, 1.5});//we'll consider m3 is Bab Touma Square
+        State.stations.put("m4", new Double[]{ 4.5, 1.5});//we'll conside m4 is Abbasiyeen Square
+        State.stations.put("m5", new Double[]{ 4.5, 1.5});//we'll consider m5 is Abbasiyeen Garages
 
         // State.edges.put
+        State.edges = new HashMap<String, HashMap<String, Edge>>();
+        HashMap<String,Edge>temp = new HashMap<String,Edge>();
+
+        //distances are from google maps (km)
+        temp.put("m2",new Edge(2.44,false,true,40,50));
+        temp.put("m3",new Edge(3.21,true,true,40,50));
+        temp.put("m4",new Edge(4.49,true,true,60,50));
+        temp.put("m5",new Edge(5.3,true,true,60,50));
+        
+        State.edges.put("m1",temp);
+        temp.clear();
+
+        temp.put("m1",new Edge(2.44,false,true,40,50));
+        temp.put("m3",new Edge(0.7,false,true,40,50));
+        temp.put("m4",new Edge(1.66,true,true,40,50));
+        temp.put("m5",new Edge(2.63,true,true,40,50));
+        
+        State.edges.put("m2",temp);
+        temp.clear();
+
+        temp.put("m1",new Edge(3.21,true,true,40,50));
+        temp.put("m2",new Edge(0.7,false,true,40,50));
+        temp.put("m4",new Edge(1.60,true,true,40,50));
+        temp.put("m5",new Edge(2.57,false,true,40,50));
+        
+        State.edges.put("m3",temp);
+        temp.clear();
+
+        temp.put("m1",new Edge(4.49,true,true,40,50));
+        temp.put("m2",new Edge(1.66,true,true,40,50));
+        temp.put("m3",new Edge(1.60,true,true,40,50));
+        temp.put("m5",new Edge(0.94,true,true,40,50));
+        
+        State.edges.put("m4",temp);
+        temp.clear();
+
+        temp.put("m1",new Edge(5.3,true,true,40,50));
+        temp.put("m2",new Edge(2.63,true,true,40,50));
+        temp.put("m3",new Edge(2.57,false,true,40,50));
+        temp.put("m4",new Edge(0.94,true,true,40,50));
+        
+        State.edges.put("m5",temp);
+        temp.clear();
+
+
     }
 }
--- src/State.java
+++ src/State.java
@@ -7,123 +7,61 @@ import java.util.Objects;
 
 public class State {
     State parent;
-    double balance;
-    double HP;
+    static float walking_speed = 5.5F;
+    static HashMap<String, Double[]> stations;   // i think float instead of Float will make some problems
+    static int bus_fee = 400;
+    static int taxi_fee = 1000;
+    static int walking_fee = 0;
+    float balance;
+    int HP;
+    static int bus_energy_cost = -5;
+    static int taxi_energy_cost = 5;
+    static int walking_energy_cost = -10;
+    static HashMap<String, HashMap<String, Edge>> edges;
     String currentStation;
 
-    static double walking_speed = 5.5F;
-    static double bus_fee = 400;
-    static double taxi_fee = 1000;
-    static double walking_fee = 0;
-    static double bus_energy_cost = -5;
-    static double taxi_energy_cost = 5;
-    static double walking_energy_cost = -10;
-    static Map<String, Map<String, Edge>> edges;
-    static Map<String, Double[]> stations = new HashMap<String, Double[]>(); // i think float instead of Float will make some problems
-
-    public State(State parent, double balance, double HP, String currentStation) {
+    public State(State parent, float balance, int HP, String currentStation) {
         this.parent = parent;
         this.balance = balance;
         this.HP = HP;
         this.currentStation = currentStation;
     }
 
-    public balance_hp_entry[] checkMoves() {
-        Map<String, Edge> moves = State.edges.get(currentStation);
+    public Map<String, Edge> checkMoves() {
+        return State.edges.get(currentStation);
+    }
 
-        balance_hp_entry[] result = new balance_hp_entry[moves.size() * 3];
+    public State[] getNextStates() {
+        Map<String, Edge> moves = checkMoves();
+        State[] result = new State[moves.size() * 3];
         int i = 0;
         for (Map.Entry<String, Edge> entry : moves.entrySet()) {
             Edge e = entry.getValue();
-            double new_hp_walking, new_hp_bus, new_hp_taxi;
-            double new_balance_walking, new_balance_bus, new_balance_taxi;
-            // new hp and new balance in case of walking to next station
-            new_hp_walking = this.calc_hp(this.HP, e.distance, "walking");
-            new_balance_walking = this.calc_balance(this.balance, e.distance, "walking");
-            // new hp and new balance in case of bus to next station
-            new_hp_bus = this.calc_hp(this.HP, e.distance, "bus");
-            new_balance_bus = this.calc_balance(this.balance, e.distance, "bus");
-            // new hp and new balance in case of taxi to next station
-            new_hp_taxi = this.calc_hp(this.HP, e.distance, "taxi");
-            new_balance_taxi = this.calc_balance(this.balance, e.distance, "taxi");
-
-            if (new_hp_walking > 0 && new_balance_walking > 0) {// you can walk (hp allows it /balance allows it))
-                result[i] = new balance_hp_entry(new_balance_walking, new_hp_walking, entry.getKey());
+            if (HP + (State.walking_energy_cost * e.distance) > 0
+                    && balance - State.walking_fee > 0) {// you can walk
+                result[i] = new State(this, balance,
+                        (int) (HP + (State.walking_energy_cost * e.distance)), entry.getKey());
                 i++;
             }
 
-            if (e.bus_route && new_hp_bus > 0 && new_balance_bus > 0) {
-                // you can take the bus (there is bus route/
-                // hp allows it /balance allows it)
-                result[i] = new balance_hp_entry(new_balance_bus, new_hp_bus, entry.getKey());
+            if (e.bus_route && HP + (State.bus_energy_cost * e.distance) > 0
+                    && balance - State.bus_fee > 0) {// you can take the bus (there is bus route/
+                                                     // hp allows it /balance allows it)
+                result[i] = new State(this, balance,
+                        (int) (HP + (State.bus_energy_cost * e.distance)), entry.getKey());
                 i++;
             }
 
-            if (e.taxi_route && new_hp_taxi > 0 && new_balance_taxi > 0) {
-                // you can take a taxi (there is taxi route/
-                // hp allows it / balance allows it)
-                result[i] = new balance_hp_entry(new_balance_taxi, new_hp_taxi, entry.getKey());
+            if (e.taxi_route && HP + (State.taxi_energy_cost * e.distance) > 0
+                    && balance - (State.taxi_fee * e.distance) > 0) {// you can take a taxi (there
+                                                                     // is taxi route/ hp allows it
+                                                                     // /balance allows it)
+                result[i] = new State(this, balance,
+                        (int) (HP + (State.taxi_energy_cost * e.distance)), entry.getKey());
                 i++;
             }
         }
         return result;
     }
 
-    public ArrayList<State> getNextStates() {
-        balance_hp_entry[] moves = checkMoves();
-        ArrayList<State> result = new ArrayList<>();
-        for (balance_hp_entry move : moves){
-            result.add(new State(this, move.balance, move.hp, move.entry));
-        }
-        return result;
-    }
-
-    public double calc_hp(double HP, double distance, String operation) {
-        if (operation.equals("walking")) {
-            double temp = HP + (State.walking_energy_cost * distance);
-            if (temp > 100)
-                return 100;
-            else
-                return temp;
-        } else if (operation.equals("bus")) {
-            double temp = HP + (State.bus_energy_cost * distance);
-            if (temp > 100)
-                return 100;
-            else
-                return temp;
-        } else if (operation.equals("taxi")) {
-            double temp = HP + (State.taxi_energy_cost * distance);
-            if (temp > 100)
-                return 100;
-            else
-                return temp;
-        } else {
-            return 0;
-        }
-    }
-
-    public double calc_balance(double balance, double distance, String operation) {
-        if (operation.equals("walking")) {
-            return balance - State.walking_fee;
-        } else if (operation.equals("bus")) {
-            return balance - State.bus_fee;
-        } else if (operation.equals("taxi")) {
-            return balance - (State.taxi_fee * distance);
-        } else {
-            return 0;
-
-        }
-    }
-
-    public class balance_hp_entry{
-        String entry;
-        double hp;
-        double balance;
-
-        public balance_hp_entry(double balance,double hp,String entry){
-            this.entry = entry;
-            this.balance = balance;
-            this.hp = hp;
-        }
-    }
 }
