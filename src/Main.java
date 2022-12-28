ed9c97d4be0bf725d0af4ae9d726b286622567b5
nicolas_alahmar <nicolasalahmar@gmail.com>
2022-12-28 17:13:56 +0300

    added time to the mix

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        State s = new State(null, 10000, 100, "m1");
+        State s = new State(null, 10000, 100, "m1", 0);
         //this.stations.put(station name, [bus waiting time, taxi waiting time])
         State.stations.put("m1", new Double[]{ 4.5, 1.5});
         State.stations.put("m2", new Double[]{ 4.5, 1.5});
--- src/State.java
+++ src/State.java
@@ -10,6 +10,7 @@ public class State {
     double balance;
     double HP;
     String currentStation;
+    double time;
 
     static double walking_speed = 5.5;
     static double bus_fee = 400;
@@ -22,48 +23,53 @@ public class State {
     static Map<String, Double[]> stations = new HashMap<String, Double[]>(); // i think float instead of Float will make some problems
     static String finalState;
 
-    public State(State parent, double balance, double HP, String currentStation) {
+    public State(State parent, double balance, double HP, String currentStation, double time) {
         this.parent = parent;
         this.balance = balance;
         this.HP = HP;
         this.currentStation = currentStation;
+        this.time = time;
     }
 
-    public balance_hp_entry[] checkMoves() {
+    public balance_hp_entry_time[] checkMoves() {
         Map<String, Edge> moves = State.edges.get(currentStation);
 
-        balance_hp_entry[] result = new balance_hp_entry[moves.size() * 3];
+        balance_hp_entry_time[] result = new balance_hp_entry_time[moves.size() * 3];
         int i = 0;
         for (Map.Entry<String, Edge> entry : moves.entrySet()) {
             Edge e = entry.getValue();
             double new_hp_walking, new_hp_bus, new_hp_taxi;
             double new_balance_walking, new_balance_bus, new_balance_taxi;
-            // new hp and new balance in case of walking to next station
+            double new_time_walking, new_time_bus, new_time_taxi;
+            // new hp, new balance and new time in case of walking to next station
             new_hp_walking = this.calc_hp(this.HP, e.distance, "walking");
             new_balance_walking = this.calc_balance(this.balance, e.distance, "walking");
-            // new hp and new balance in case of bus to next station
+            new_time_walking = this.calc_time(e.distance, State.walking_speed, "walking");
+            // new hp, new balance and new time in case of bus to next station
             new_hp_bus = this.calc_hp(this.HP, e.distance, "bus");
             new_balance_bus = this.calc_balance(this.balance, e.distance, "bus");
-            // new hp and new balance in case of taxi to next station
+            new_time_bus = this.calc_time(e.distance, e.bus_speed, "bus");
+            // new hp, new balance and new time in case of taxi to next station
             new_hp_taxi = this.calc_hp(this.HP, e.distance, "taxi");
             new_balance_taxi = this.calc_balance(this.balance, e.distance, "taxi");
+            new_time_taxi = this.calc_time(e.distance, e.taxi_speed, "taxi");
 
             if (new_hp_walking > 0 && new_balance_walking > 0) {// you can walk (hp allows it /balance allows it))
-                result[i] = new balance_hp_entry(new_balance_walking, new_hp_walking, entry.getKey());
+                result[i] = new balance_hp_entry_time(new_balance_walking, new_hp_walking, entry.getKey(), new_time_walking);
                 i++;
             }
 
             if (e.bus_route && new_hp_bus > 0 && new_balance_bus > 0) {
                 // you can take the bus (there is bus route/
                 // hp allows it /balance allows it)
-                result[i] = new balance_hp_entry(new_balance_bus, new_hp_bus, entry.getKey());
+                result[i] = new balance_hp_entry_time(new_balance_bus, new_hp_bus, entry.getKey(), new_time_bus);
                 i++;
             }
 
             if (e.taxi_route && new_hp_taxi > 0 && new_balance_taxi > 0) {
                 // you can take a taxi (there is taxi route/
                 // hp allows it / balance allows it)
-                result[i] = new balance_hp_entry(new_balance_taxi, new_hp_taxi, entry.getKey());
+                result[i] = new balance_hp_entry_time(new_balance_taxi, new_hp_taxi, entry.getKey(), new_time_taxi);
                 i++;
             }
         }
@@ -71,10 +77,10 @@ public class State {
     }
 
     public ArrayList<State> getNextStates() {
-        balance_hp_entry[] moves = checkMoves();
+        balance_hp_entry_time[] moves = checkMoves();
         ArrayList<State> result = new ArrayList<>();
-        for (balance_hp_entry move : moves){
-            result.add(new State(this, move.balance, move.hp, move.entry));
+        for (balance_hp_entry_time move : moves){
+            result.add(new State(this, move.balance, move.hp, move.entry, move.time));
         }
         return result;
     }
@@ -116,20 +122,36 @@ public class State {
         }
     }
 
-    public class balance_hp_entry{
+    public double calc_time(double distance, double speed, String operation) {
+        if (operation.equals("walking")) {
+            return this.time + (distance/speed);
+        } else if (operation.equals("bus")) {
+            // the time already gone + time of waiting + time of travelling
+            return this.time + State.stations.get(this.currentStation)[0] + (distance/speed);
+        } else if (operation.equals("taxi")) {
+            // the time already gone + time of waiting + time of travelling
+            return this.time + State.stations.get(this.currentStation)[1] + (distance/speed);
+        } else {
+            return 0;
+        }
+    }
+
+    public class balance_hp_entry_time{
         String entry;
         double hp;
         double balance;
+        double time;
 
-        public balance_hp_entry(double balance,double hp,String entry){
+        public balance_hp_entry_time(double balance,double hp,String entry, double time){
             this.entry = entry;
             this.balance = balance;
             this.hp = hp;
+            this.time = time;
         }
     }
 
     public boolean equals(State o2){
-        boolean[] arr = {this.balance == o2.balance, this.HP == o2.HP, this.currentStation.equals(o2.currentStation), this.parent == o2.parent};
+        boolean[] arr = {this.balance == o2.balance, this.HP == o2.HP, this.currentStation.equals(o2.currentStation), this.parent == o2.parent, this.time == o2.time};
         for(boolean b : arr) if(!b) return false;
         return true;
     }
