09814437efa9abb34f57880d5e856ddafbea8603
ritarezek200185 <ritarezek200185@gmail.com>
2023-01-01 19:47:50 +0300

    json reading (done) starting json validation..

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        State s = new State(null,10000, 0, 100, "Hamak", 0, "");
-        //this.stations.put(station name, [bus waiting time, taxi waiting time])
-        Main.func();
-
+       // State s = new State(null,10000, 0, 100, "Hamak", 0, "");
+       // Main.func();
+        ReadJson json = new ReadJson();
+        json.read();
+        State s = json.s;
         Astar a = new Astar("fastestTime", s);
 
         double start = System.nanoTime();
@@ -20,85 +21,76 @@ public class Main {
         System.out.println("number of nodes instantiated: "+a.n);
         System.out.println("time of execution: "+((finish - start)/1000000)+" ms");
 
-        // t.printPath();
-
-        // System.out.println(State.edges.get(s.currentStation));
-        
-
-        // ReadJson json = new ReadJson();
-        // json.read();
-
-        // State.edges.put
     }
 
-    public static void func(){
-        State.stations.put("Hamak", new Double[]{ 0.2, 0.1});
-        State.stations.put("Bab Touma", new Double[]{ 0.3, 0.2});
-        State.stations.put("Bab sharqui", new Double[]{ 0.4,0.5});
-        State.stations.put("Abbasyian", new Double[]{ 0.5, 0.6});
-        State.stations.put("Kassaa", new Double[]{ 0.4, 0.3});
-        
-        State.walking_speed = 5.5;
-        State.bus_fee = 400;
-        State.taxi_fee = 1000;
-        State.walking_fee = 0;
-        State.bus_energy_cost = -5;
-        State.taxi_energy_cost = 5;
-        State.walking_energy_cost = -10;
-        State.finalState = "Kassaa";
+    public static void func() {
+//        State.stations.put("Hamak", new Double[]{ 0.2, 0.1});
+//        State.stations.put("Bab Touma", new Double[]{ 0.3, 0.2});
+//        State.stations.put("Bab sharqui", new Double[]{ 0.4,0.5});
+//        State.stations.put("Abbasyian", new Double[]{ 0.5, 0.6});
+//        State.stations.put("Kassaa", new Double[]{ 0.4, 0.3});
 
-        Map<String, Edge>temp = new HashMap<>();
+//        State.walking_speed = 5.5;
+//        State.bus_fee = 400;
+//        State.taxi_fee = 1000;
+//        State.walking_fee = 0;
+//        State.bus_energy_cost = -5;
+//        State.taxi_energy_cost = 5;
+//        State.walking_energy_cost = -10;
+//        State.finalState = "Kassaa";
 
-        temp.put("Bab sharqui", new Edge(5, true, false,40,50,"hamak - bab sharqui"));
-        temp.put("Bab Touma", new Edge(7,true,false,40,50,"hamak - bab touma"));
-        temp.put("Abbasyian", new Edge(9, false, false, 40, 50, "hamak - Abbasyian"));
-        temp.put("Kassaa", new Edge(12, false, true, 40, 50, "hamak - Kassaa"));
-
-        State.edges.put("Hamak", temp);
-
-
-
-
-        Map<String, Edge>temp2 = new HashMap<>();
-
-        temp2.put("Hamak", new Edge(5, false, false, 40, 50, "Bab sharqui - Hamak"));
-        temp2.put("Bab Touma", new Edge(3, false, true, 40, 50, "Bab sharqui - Bab Touma"));
-        temp2.put("Abbasyian", new Edge(5, false, true, 40, 50, "Bab sharqui - Abbasyian"));
-        temp2.put("Kassaa", new Edge(12, false, false, 40, 50, "Bab sharqui - Kassaa"));
-
-        State.edges.put("Bab sharqui", temp2);
-
-
-
-        Map<String, Edge>temp3 = new HashMap<>();
-
-        temp3.put("Hamak", new Edge(7, false, false, 40, 50, "Bab Touma - Hamak"));
-        temp3.put("Bab sharqui", new Edge(3, true, true, 40, 50, "Bab Touma - bab sharqui"));
-        temp3.put("Abbasyian", new Edge(2, true, false, 40, 50, "Bab Touma - Abbasyian"));
-        temp3.put("Kassaa", new Edge(10, false, false, 40, 50, "Bab Touma - Kassaa"));
-
-        State.edges.put("Bab Touma", temp3);
-
-
-
-
-        Map<String, Edge>temp4 = new HashMap<>();
-
-        temp4.put("Hamak", new Edge(9, false, false, 40, 50, "Abbasyian - Hamak"));
-        temp4.put("Bab sharqui", new Edge(5, true, false, 40, 50, "Abbasyian - bab sharqui"));
-        temp4.put("Bab Touma", new Edge(2, false, false, 40, 50, "Abbasyian - Bab Touma"));
-        temp4.put("Kassaa", new Edge(1, false, false, 40, 50, "Abbasyian - Kassaa"));
-
-        State.edges.put("Abbasyian", temp4);
-
-
-        Map<String, Edge>temp5 = new HashMap<>();
-
-        temp5.put("Hamak", new Edge(12, false, false, 40, 50, "Kassaa - Hamak"));
-        temp5.put("Bab sharqui", new Edge(12, false, false, 40, 50, "Kassaa - bab sharqui"));
-        temp5.put("Bab Touma", new Edge(10, false, false, 40, 50, "Kassaa - Bab Touma"));
-        temp5.put("Abbasyian", new Edge(1, false, false, 40, 50, "Kassaa - Abbasyian"));
-
-        State.edges.put("Kassaa", temp5);
+//        Map<String, Edge>temp = new HashMap<>();
+//
+//        temp.put("Bab sharqui", new Edge(5, true, false,40,50,"hamak - bab sharqui"));
+//        temp.put("Bab Touma", new Edge(7,true,false,40,50,"hamak - bab touma"));
+//        temp.put("Abbasyian", new Edge(9, false, false, 40, 50, "hamak - Abbasyian"));
+//        temp.put("Kassaa", new Edge(12, false, true, 40, 50, "hamak - Kassaa"));
+//
+//        State.edges.put("Hamak", temp);
+//
+//
+//
+//
+//        Map<String, Edge>temp2 = new HashMap<>();
+//
+//        temp2.put("Hamak", new Edge(5, false, false, 40, 50, "Bab sharqui - Hamak"));
+//        temp2.put("Bab Touma", new Edge(3, false, true, 40, 50, "Bab sharqui - Bab Touma"));
+//        temp2.put("Abbasyian", new Edge(5, false, true, 40, 50, "Bab sharqui - Abbasyian"));
+//        temp2.put("Kassaa", new Edge(12, false, false, 40, 50, "Bab sharqui - Kassaa"));
+//
+//        State.edges.put("Bab sharqui", temp2);
+//
+//
+//
+//        Map<String, Edge>temp3 = new HashMap<>();
+//
+//        temp3.put("Hamak", new Edge(7, false, false, 40, 50, "Bab Touma - Hamak"));
+//        temp3.put("Bab sharqui", new Edge(3, true, true, 40, 50, "Bab Touma - bab sharqui"));
+//        temp3.put("Abbasyian", new Edge(2, true, false, 40, 50, "Bab Touma - Abbasyian"));
+//        temp3.put("Kassaa", new Edge(10, false, false, 40, 50, "Bab Touma - Kassaa"));
+//
+//        State.edges.put("Bab Touma", temp3);
+//
+//
+//
+//
+//        Map<String, Edge>temp4 = new HashMap<>();
+//
+//        temp4.put("Hamak", new Edge(9, false, false, 40, 50, "Abbasyian - Hamak"));
+//        temp4.put("Bab sharqui", new Edge(5, true, false, 40, 50, "Abbasyian - bab sharqui"));
+//        temp4.put("Bab Touma", new Edge(2, false, false, 40, 50, "Abbasyian - Bab Touma"));
+//        temp4.put("Kassaa", new Edge(1, false, false, 40, 50, "Abbasyian - Kassaa"));
+//
+//        State.edges.put("Abbasyian", temp4);
+//
+//
+//        Map<String, Edge>temp5 = new HashMap<>();
+//
+//        temp5.put("Hamak", new Edge(12, false, false, 40, 50, "Kassaa - Hamak"));
+//        temp5.put("Bab sharqui", new Edge(12, false, false, 40, 50, "Kassaa - bab sharqui"));
+//        temp5.put("Bab Touma", new Edge(10, false, false, 40, 50, "Kassaa - Bab Touma"));
+//        temp5.put("Abbasyian", new Edge(1, false, false, 40, 50, "Kassaa - Abbasyian"));
+//
+//        State.edges.put("Kassaa", temp5);
     }
 }
--- src/ReadJson.java
+++ src/ReadJson.java
@@ -7,13 +7,32 @@ import java.util.*;
 
 
 public class ReadJson {
-
+    State s;
     public void read() {
         JSONParser parser = new JSONParser();
         ArrayList<Map<String, Edge>> maps = new ArrayList<>();
+
         try {
             Object obj = parser.parse(new FileReader("text.json"));
             JSONObject jsonObject = (JSONObject) obj;
+            JSONObject state = (JSONObject) jsonObject.get("state");
+            State parent = (State) state.get("parent");
+            double balance = ((Number) state.get("balance")).doubleValue();
+            double spent_money = ((Number) state.get("spent_money")).doubleValue();
+            double HP = ((Number) state.get("HP")).doubleValue();
+            String currentStation = (String) state.get("currentStation");
+            double time = ((Number) state.get("time")).doubleValue();
+            String operation = (String) state.get("operation");
+
+            State.walking_speed =((Number) state.get("walking_speed")).doubleValue();
+            State.bus_fee = ((Number) state.get("bus_fee")).doubleValue();
+            State.taxi_fee = ((Number) state.get("taxi_fee")).doubleValue();
+            State.walking_fee =((Number) state.get("walking_fee")).doubleValue();
+            State.bus_energy_cost =((Number) state.get("bus_energy_cost")).doubleValue();
+            State.taxi_energy_cost = ((Number) state.get("taxi_energy_cost")).doubleValue();
+            State.walking_energy_cost =((Number) state.get("walking_energy_cost")).doubleValue();
+            State.finalState = (String) state.get("finalState");
+            s = new State(parent, balance, spent_money, HP, currentStation, time, operation);
             JSONObject stations = (JSONObject) jsonObject.get("stations");
             JSONObject edges = (JSONObject) jsonObject.get("edges");
             Set keySet1 = edges.keySet();
@@ -39,7 +58,7 @@ public class ReadJson {
 
                     JSONArray edge_array = (JSONArray) edge;
                     Edge edge_obj = new Edge(
-                            ((Number) edge_array.get(0)).floatValue(),
+                            ((Number) edge_array.get(0)).doubleValue(),
                             (Boolean) edge_array.get(1),
                             (Boolean) edge_array.get(2),
                             ((Number) edge_array.get(3)).floatValue(),
@@ -60,10 +79,6 @@ public class ReadJson {
             for (int i = 0; i < start_points.length; i++) {
                 State.edges.put((String) start_points[i], maps.get(i));
             }
-//            for (Object s:State.edges.entrySet()) {
-//                System.out.println(((JSONObject)s).keySet());
-//                System.out.println(((JSONObject)s).keySet());
-//            }
 
         } catch (Exception e) {
             e.printStackTrace();
